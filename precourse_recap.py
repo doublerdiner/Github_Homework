26bd2f207de649d88bac1f8385a8771aeaef73c4
Chris Barclay <chris.barclay1@gmail.com>
2023-01-23 17:19:11 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+name = "Chris Barclay"
+age = 34
+years_in_the_future = 5
+
+def individual_details(name, age, years_in_the_future):
+    return(f"Hi, my name is {name} and I'm {age} years old. \nIn {years_in_the_future} years time I'll be {age + years_in_the_future} years old.")
+
+print(individual_details(name, age, years_in_the_future))
