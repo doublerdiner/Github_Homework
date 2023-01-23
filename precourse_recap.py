4bfafd754bc7310fcc085c3331cb9458de1ca15b
Chris Barclay <chris.barclay1@gmail.com>
2023-01-23 17:33:16 +0000

    Changed the code. Added a boolean element to the code.

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,8 +1,15 @@
 name = "Chris Barclay"
 age = 34
+has_a_dog = True
 years_in_the_future = 5
 
-def individual_details(name, age, years_in_the_future):
-    return(f"Hi, my name is {name} and I'm {age} years old. \nIn {years_in_the_future} years time I'll be {age + years_in_the_future} years old.")
 
-print(individual_details(name, age, years_in_the_future))
+def individual_details(name, age, has_a_dog, years_in_the_future):
+    statement = (f"Hi, my name is {name} and I'm {age} years old. \
+        \nIn {years_in_the_future} years time I'll be {age + years_in_the_future} years old.")
+    if has_a_dog:
+        return statement + (f"\nOh... And I also have a dog too.")
+    else: 
+        return statement
+
+print(individual_details(name, age, has_a_dog, years_in_the_future))
